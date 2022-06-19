88d3e7aead35f1209c1b45f1420d89074827de51
Alfian Andi Nugraha <alfian.23andi@gmail.com>
2022-06-19 19:57:31 +0700

    feat(pages/home): init home content

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,8 +1,30 @@
 import BaseLayout from "@/layouts/base-layout";
+import { Box, Typography } from "@mui/material";
 import React from "react";
+import MusicNoteIcon from "@heroicons/react/solid/MusicNoteIcon";
+import { green } from "@mui/material/colors";
 
 const HomePage = () => {
-  return <BaseLayout>HomePage</BaseLayout>;
+  return (
+    <BaseLayout>
+      <Box pt="2rem" textAlign="center">
+        <MusicNoteIcon
+          width={98}
+          height={98}
+          style={{
+            color: green[600],
+          }}
+        />
+        <Typography variant="h5">
+          Welcome to{" "}
+          <Typography variant="inherit" color={green[600]} display="inline">
+            Musicshot
+          </Typography>
+        </Typography>
+        <Typography>Play your favourite song here.</Typography>
+      </Box>
+    </BaseLayout>
+  );
 };
 
 export default HomePage;
