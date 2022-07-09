3fbc06b47777c2abe75149d8d50e06531b6d5831
Alfian Andi Nugraha <alfian.23andi@gmail.com>
2022-07-09 12:18:27 +0700

    feat(pages/playlist): fetch all track

---
+++ src/components/track-card.tsx
@@ -0,0 +1,50 @@
+import PlayIcon from "@heroicons/react/solid/PlayIcon";
+import { Box, Button, Typography } from "@mui/material";
+
+type TrackCardProps = {
+  id: string;
+  title: string;
+  artist: string;
+  imageUrl: string;
+};
+
+const TrackCard = (props: TrackCardProps) => {
+  return (
+    <Box
+      marginBottom="1rem"
+      pl="1rem"
+      boxShadow="rgba(0, 0, 0, 0.1) 0px 4px 6px -1px, rgba(0, 0, 0, 0.06) 0px 2px 4px -1px;"
+      borderRadius="4px"
+    >
+      <Box display="flex">
+        <Box pr="1rem" py="1rem">
+          <Box>
+            <Typography sx={{ fontWeight: "bold" }}>{props.title}</Typography>
+            <Typography variant="caption">{props.artist}</Typography>
+          </Box>
+          <Button
+            variant="contained"
+            startIcon={<PlayIcon style={{ width: "1rem", height: "1rem" }} />}
+            sx={{
+              mt: ".5rem",
+            }}
+          >
+            Play
+          </Button>
+        </Box>
+        <Box
+          component="img"
+          src={props.imageUrl}
+          borderRadius="0 4px 4px 0"
+          maxWidth="120px"
+          ml="auto"
+          sx={{
+            objectFit: "cover",
+          }}
+        />
+      </Box>
+    </Box>
+  );
+};
+
+export default TrackCard;
--- src/pages/playlist.tsx
+++ src/pages/playlist.tsx
@@ -1,8 +1,32 @@
+import TrackCard from "@/components/track-card";
 import BaseLayout from "@/layouts/base-layout";
-import React from "react";
+import { useGetAllTopQuery } from "@/store/apis/trackApi";
+import { Typography } from "@mui/material";
+import { Box } from "@mui/system";
 
 const PlaylistPage = () => {
-  return <BaseLayout>PlaylistPage</BaseLayout>;
+  const { data: tracks = [] } = useGetAllTopQuery();
+  console.log(tracks);
+  return (
+    <BaseLayout>
+      <Typography variant="h5" sx={{ mt: "1rem" }}>
+        Playlist
+      </Typography>
+      <Box mb="5rem" mt="1rem">
+        {tracks.map((track) => {
+          return (
+            <TrackCard
+              key={track.id}
+              artist={track.artistName}
+              id={track.id}
+              title={track.albumName}
+              imageUrl={`https://api.napster.com/imageserver/v2/albums/${track.albumId}/images/300x300.jpg`}
+            />
+          );
+        })}
+      </Box>
+    </BaseLayout>
+  );
 };
 
 export default PlaylistPage;
