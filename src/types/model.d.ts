95474693fb06da9d46cf56fc49d74f3c0f396b44
Alfian Andi Nugraha <alfian.23andi@gmail.com>
2022-07-09 12:09:35 +0700

    feat(types/model): create model and Track type

---
+++ src/types/model.d.ts
@@ -0,0 +1,10 @@
+declare module "model" {
+  export type Track = {
+    previewURL: string;
+    albumId: string;
+    artistName: string;
+    albumName: string;
+    name: string;
+    id: string;
+  };
+}
