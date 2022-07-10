bd2e15cc1ae6eeae97d8d4efdaa5893eb8198f5c
Alfian Andi Nugraha <alfian.23andi@gmail.com>
2022-07-10 12:47:19 +0700

    feat(pages/playlist): add loading and error state

--- src/pages/playlist.tsx
+++ src/pages/playlist.tsx
@@ -4,12 +4,17 @@ import BaseLayout from "@/layouts/base-layout";
 import { useAppSelector } from "@/store";
 import { useGetAllTopQuery } from "@/store/apis/trackApi";
 import trackSlice from "@/store/slice/trackSlice";
-import { Typography } from "@mui/material";
+import { Button, CircularProgress, Typography } from "@mui/material";
 import { Box } from "@mui/system";
 import { useDispatch } from "react-redux";
 
 const PlaylistPage = () => {
-  const { data: tracks = [] } = useGetAllTopQuery();
+  const {
+    data: tracks = [],
+    isSuccess,
+    isLoading,
+    refetch,
+  } = useGetAllTopQuery();
   const trackId = useAppSelector((value) => value.track.currentTrack?.id);
   const { play } = useAudioElement();
   const dispatch = useDispatch();
@@ -20,22 +25,47 @@ const PlaylistPage = () => {
         Playlist
       </Typography>
       <Box mb="5rem" mt="1rem">
-        {tracks.map((track) => {
-          return (
-            <TrackCard
-              key={track.id}
-              artist={track.artistName}
-              id={track.id}
-              title={track.albumName}
-              isPlaying={trackId === track.id}
-              imageUrl={`https://api.napster.com/imageserver/v2/albums/${track.albumId}/images/300x300.jpg`}
-              onClickPlay={() => {
-                play(track.previewURL);
-                dispatch(trackSlice.actions.start(track));
-              }}
-            />
-          );
-        })}
+        {isLoading ? (
+          <Box display="flex" flexDirection="column" alignItems="center">
+            <CircularProgress />
+            <Typography sx={{ mt: "1rem" }}>Getting all track</Typography>
+          </Box>
+        ) : (
+          <>
+            {isSuccess ? (
+              <>
+                {tracks.map((track) => {
+                  return (
+                    <TrackCard
+                      key={track.id}
+                      artist={track.artistName}
+                      id={track.id}
+                      title={track.albumName}
+                      isPlaying={trackId === track.id}
+                      imageUrl={`https://api.napster.com/imageserver/v2/albums/${track.albumId}/images/300x300.jpg`}
+                      onClickPlay={() => {
+                        play(track.previewURL);
+                        dispatch(trackSlice.actions.start(track));
+                      }}
+                    />
+                  );
+                })}
+              </>
+            ) : (
+              <Box display="flex" flexDirection="column" alignItems="center">
+                <Typography>Failed to get data</Typography>
+                <Button
+                  variant="contained"
+                  color="error"
+                  sx={{ mt: "1rem" }}
+                  onClick={refetch}
+                >
+                  Retry
+                </Button>
+              </Box>
+            )}
+          </>
+        )}
       </Box>
     </BaseLayout>
   );
